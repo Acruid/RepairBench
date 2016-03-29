2b19bc92b7fc5b070f744b3829f9fc9189652814
Acruid <shatter66@gmail.com>
2016-03-29 10:00:00 -0700

    Updated to game version 0.12.914 rev777.

---
+++ Source/JobDriver_MendItem.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Mending
+{
+    // ReSharper disable once InconsistentNaming
+    internal class JobDriver_MendItem : JobDriver
+    {
+        private const float REPAIR_RATE = 12f; // game ticks per reptick
+        private const float SKILL_GAIN = 0.55f; // Skill gain per reptick
+        private const int HP_GAIN = 1; // durability regen per reptick
+
+        private const TargetIndex TI_REPBENCH = TargetIndex.A;
+        private const TargetIndex TI_ITEM = TargetIndex.B;
+        private const TargetIndex TI_CELL = TargetIndex.C;
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            this.FailOnDestroyedOrForbidden(TI_REPBENCH);
+            this.FailOnBurningImmobile(TI_REPBENCH);
+
+            this.FailOnDestroyedOrForbidden(TI_ITEM);
+            this.FailOnBurningImmobile(TI_ITEM);
+
+            yield return Toils_Reserve.Reserve(TI_REPBENCH);
+
+            yield return Toils_Reserve.Reserve(TI_ITEM);
+
+            yield return Toils_Goto.GotoCell(TI_ITEM, PathEndMode.OnCell);
+
+            yield return Toils_Haul.StartCarryThing(TI_ITEM);
+
+            yield return Toils_Goto.GotoThing(TI_REPBENCH, PathEndMode.InteractionCell);
+
+            yield return Toils_Haul.PlaceHauledThingInCell(TI_REPBENCH, null, false);
+
+            var item = pawn.CurJob.GetTarget(TI_ITEM).Thing;
+            var ticksToNextRepair = REPAIR_RATE;
+            var repairToil = new Toil
+            {
+                tickAction = () =>
+                {
+                    pawn.skills.Learn(SkillDefOf.Crafting, SKILL_GAIN);
+                    ticksToNextRepair -= pawn.GetStatValue(StatDefOf.WorkSpeedGlobal);
+
+                    if (ticksToNextRepair > 0.0)
+                        return;
+
+                    ticksToNextRepair = REPAIR_RATE;
+                    item.HitPoints += HP_GAIN;
+
+                    if (item.HitPoints < item.MaxHitPoints)
+                        return;
+
+                    // break
+                    ReadyForNextToil();
+                },
+                defaultCompleteMode = ToilCompleteMode.Never
+            };
+            repairToil.WithEffect(item.def.repairEffect, TI_ITEM);
+            yield return repairToil;
+
+            /*
+
+                        var toilColor = new Toil();
+                        TargetInfo target3 = target1;
+                        toilColor.initAction = () =>
+                        {
+                            //Color the Apparel
+                            if (target3.Thing is Apparel)
+                            {
+                                var clothing = target3.Thing as Apparel;
+            //                if(clothing.Stuff.defName == "Cloth")
+                                var comp = clothing.GetComp<CompColorable>();
+
+                                if (comp == null || !comp.Active)
+                                    return;
+
+                                // set it to default color
+                                comp.Color = comp.parent.def.defaultColor;
+
+                                // turn off the color comp
+                                FieldInfo fieldInfo = typeof (CompColorable).GetField("active", BindingFlags.Instance | BindingFlags.NonPublic);
+                                if (fieldInfo != null)
+                                    fieldInfo.SetValue(comp, false);
+                            }
+                        };
+                        toilColor.defaultCompleteMode = (ToilCompleteMode) 1;
+                        yield return toilColor;
+            */
+
+            yield return Toils_Haul.StartCarryThing(TI_ITEM);
+
+            if (pawn.CurJob.GetTarget(TI_CELL).Cell.IsValidStorageFor(item))
+            {
+                yield return Toils_Reserve.Reserve(TI_CELL);
+
+                yield return Toils_Haul.CarryHauledThingToCell(TI_CELL);
+
+                yield return Toils_Haul.PlaceHauledThingInCell(TI_CELL, null, true);
+
+                yield return Toils_Reserve.Release(TI_CELL);
+            }
+            else
+            {
+                var dropObjToil = new Toil
+                {
+                    initAction = () =>
+                    {
+                        Thing resultingThing;
+                        pawn.carrier.TryDropCarriedThing(pawn.Position, ThingPlaceMode.Direct, out resultingThing);
+                    },
+                    defaultCompleteMode = ToilCompleteMode.Instant
+                };
+                yield return dropObjToil;
+            }
+
+            yield return Toils_Reserve.Release(TI_REPBENCH);
+        }
+    }
+}
---
+++ Source/WorkGiver_Mending.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Mending
+{
+    // ReSharper disable once InconsistentNaming
+    internal class WorkGiver_Mending : WorkGiver_Scanner
+    {
+        private MenderBuildingComp _mbc;
+
+        public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(ThingDef.Named("TableMender"));
+
+        public override Job JobOnThing(Pawn menderPawn, Thing menderTableThing)
+        {
+            var buildingWorkTable = menderTableThing as Building_WorkTable;
+
+            if (buildingWorkTable == null)
+                return null;
+
+            _mbc = menderTableThing.TryGetComp<MenderBuildingComp>();
+
+            if (_mbc == null || !menderPawn.CanReserveAndReach(menderTableThing, PathEndMode.Touch, menderPawn.NormalMaxDanger()))
+                return null;
+
+            var compPowerTrader = (buildingWorkTable).GetComp<CompPowerTrader>();
+            if (compPowerTrader != null && !compPowerTrader.PowerOn)
+                return null;
+            try
+            {
+                Thing thing = GenClosest.ClosestThingReachable(menderTableThing.Position,
+                    ThingRequest.ForGroup((ThingRequestGroup)4),
+                    PathEndMode.Touch,
+                    TraverseParms.For(menderPawn, menderPawn.NormalMaxDanger()),
+                    _mbc.SearchRadius / 2f,
+                    SearchPredicate);
+
+                if (thing == null)
+                    return null;
+
+                IntVec3 invalid;
+                if (
+                    !StoreUtility.TryFindBestBetterStoreCellFor(thing, menderPawn, 0, menderPawn.Faction, out invalid))
+                    invalid = IntVec3.Invalid;
+                else
+                    menderPawn.Reserve(invalid);
+
+                var job = new Job(DefDatabase<JobDef>.GetNamed("MendItem"), menderTableThing, thing)
+                {
+                    maxNumToCarry = 1
+                };
+
+                if (invalid != IntVec3.Invalid)
+                    job.targetC = invalid;
+
+                return job;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Mending mod: JobOnThing: " + ex.Message);
+            }
+            return null;
+        }
+
+        private bool SearchPredicate(Thing t)
+        {
+            try
+            {
+                if (!_mbc.GetAllowances().Allows(t))
+                    return false;
+
+                if (t.HitPoints <= 0 || t.HitPoints >= t.MaxHitPoints)
+                    return false;
+
+                if (Find.Reservations.FirstReserverOf(t, Faction.OfColony) != null)
+                    return false;
+
+                if (t.IsForbidden(Faction.OfColony))
+                    return false;
+
+                if (t.IsBurning())
+                    return false;
+
+                if (!_mbc.OutsideItems && !Find.RoofGrid.Roofed(t.Position))
+                    return false;
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Mending mod: SearchPredicate: " + ex.Message);
+            }
+            return false;
+        }
+    }
+}
