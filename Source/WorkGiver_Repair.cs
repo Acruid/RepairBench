ccd493bc36c4b9237dfcdbacae6014315bf4d961
Acruid <shatter66@gmail.com>
2018-10-17 12:21:36 -0700

    Fix: Attempted to fix reservation errors by re-adding the CanReserve check in the WorkGiver.

--- Source/WorkGiver_Repair.cs
+++ Source/WorkGiver_Repair.cs
@@ -69,7 +69,10 @@ namespace Repair
 
             if (giver == null || !ThingIsUsableBillGiver(bench) || !giver.CurrentlyUsableForBills() || !giver.BillStack.AnyShouldDoNow || bench.IsBurning() || bench.IsForbidden(pawn))
                 return null;
-            
+
+            if (!pawn.CanReserve(bench))
+                return null;
+
             if (!pawn.CanReserveAndReach(bench.InteractionCell, PathEndMode.OnCell, Danger.Some))
                 return null;
             
